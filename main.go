3707771bb8b89de55040fd621aeabebdc36a4550
louiehdev <jlouiehagen@gmail.com>
2025-09-21 20:48:58 -0600

    added command structure and registry

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func cleanInput(text string) []string {
 	var cleanStrings []string
 	stringSlice := strings.Fields(text)
@@ -17,19 +23,52 @@ func cleanInput(text string) []string {
 	return cleanStrings
 }
 
+func registerCommands() map[string]cliCommand {
+	supportedCommands := make(map[string]cliCommand)
+	supportedCommands["exit"] = cliCommand{
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	}
+	supportedCommands["help"] = cliCommand{
+		name:        "help",
+		description: "Show list and usage of commands",
+		callback:    commandHelp,
+	}
+	return supportedCommands
+}
+
+func commandExit() error {
+	fmt.Print("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
+	commandRegistry := registerCommands()
+	for _, command := range commandRegistry {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	commandRegistry := registerCommands()
 
 	for {
 		fmt.Print("Pokedex >")
 		scanner.Scan()
 		input := scanner.Text()
 		cleanedInput := cleanInput(input)
-		if len(cleanedInput) == 0 {
-			fmt.Print("ERROR: No input detected!\n")
+
+		command, exists := commandRegistry[cleanedInput[0]]
+		if exists {
+			command.callback()
+		} else {
+			fmt.Print("Unknown command\n")
 			continue
 		}
-		fmt.Printf("Your command was: %v\n", cleanedInput[0])
-
 	}
 }
